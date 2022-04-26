a2367a7ab04686ef252c5329f847c393ecf5546a
jmarques <jmarques@MacBook-Air.home>
2022-04-26 19:47:59 +0200

    component Button

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,26 @@
 <script>
 	import MAinHeader from '../src/Components/Header/MAinHeader.svelte';
+	import Button from '../src/Components/atoms/Button/Button.svelte';
 	export let name;
-	let surname = "Print String"
+	let surname = "Print String";
+
+	let srcImage = '/images/rick.gif';
+	let titleImage = 'Rick Astley';
+
+	// Render HTML
+	let stringHTML = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 
 <main>
 	<MAinHeader/>
 	<h1>Hello {name}!</h1>
-	<p>{surname}</p>
+	<p>{surname.toUpperCase()}</p>
+	<img src={srcImage} alt="{titleImage} dances." />
+
+	<!-- Render HTML -->
+	<p>{@html stringHTML}</p>
+
+	<Button/>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -30,5 +43,11 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	p {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
 	}
 </style>
